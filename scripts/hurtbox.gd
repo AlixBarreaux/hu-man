bc3f68f93792b07b280cb950db80cdb5b1f50ad9
Alix <alix@github.com>
2024-03-13 16:00:09 +0100

    Feat: Add en/disable methods

--- scripts/hurtbox.gd
+++ scripts/hurtbox.gd
@@ -5,12 +5,20 @@ class_name HurtBox
 @export var actor_to_hurt: Node2D = null
 
 
+func enable() -> void:
+	for collision_shape in self.get_children():
+		collision_shape.call_deferred("set_disabled", false)
+
+
+func disable() -> void:
+	for collision_shape in self.get_children():
+		collision_shape.call_deferred("set_disabled", true)
+
+
 func _ready() -> void:
 	assert(self.actor_to_hurt != null)
 
 
 func _on_area_entered(area: Area2D) -> void:
-	for collision_shape in self.get_children():
-		collision_shape.call_deferred("set_disabled", true)
-	
+	self.disable()
 	self.actor_to_hurt.die()
