6555b5962605a7ab99b579b606c75c42594b0337
Alix <alix@github.com>
2024-03-19 00:20:40 +0100

    Feat: Add blinking label
    Also make version label do nothing special but display the version in project settings instead.

--- scripts/ui/version_label.gd
+++ scripts/ui/version_label.gd
@@ -2,27 +2,6 @@ extends Label
 class_name VersionLabel
 
 
-var modulation_alpha: int = self.modulate.a
-var use_full_opacity: bool = true
-@onready var tween: Tween = null
-
-
-func on_tween_finished() -> void:
-	tween = create_tween()
-	
-	if use_full_opacity:
-		modulation_alpha = 1.0
-	else:
-		modulation_alpha = 0.0
-	
-	use_full_opacity = not use_full_opacity
-	
-	tween.finished.connect(on_tween_finished)
-	tween.tween_property(self, "modulate:a", modulation_alpha, 1)
-
-
 func _ready() -> void:
-	tween = create_tween()
-	tween.finished.connect(on_tween_finished)
-	tween.tween_property(self, "modulate:a", 0.0, 1)
-	use_full_opacity = true
+	var version_identifier: String = ProjectSettings.get_setting("application/config/version")
+	self.set_text("Version: " + version_identifier)
