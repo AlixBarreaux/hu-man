63810486e7c371259f0ef5438f1c1938ca8c444b
Alix <alix@github.com>
2024-03-19 15:39:02 +0100

    Feat: Change level cleared text
    To something shorter because the panel is overflowed by text

--- scripts/ui/info_message_ui.gd
+++ scripts/ui/info_message_ui.gd
@@ -33,8 +33,7 @@ func on_game_over() -> void:
 
 
 func on_level_cleared() -> void:
-	var text: String = """Level cleared!
-	You completed the game, thanks for playing!"""
+	var text: String = "Level cleared! Thanks for playing!"
 	self.set_text_with_color(text, self.level_cleared_color)
 
 
