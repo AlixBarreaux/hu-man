3a4be58975f5ee6123d54b68a2a42bbd48e6ebb1
Alix <alix@github.com>
2024-03-20 19:10:08 +0100

    Feat: Switch scene when game ends
    To main menu by default or next level

--- level.gd
+++ level.gd
@@ -6,10 +6,20 @@ class_name Level
 @export var id: int = 0
 @export_file var next_level_to_load_file_path: String = ""
 
+@onready var switch_to_next_level_timer: Timer = $SwitchToNextLevelTimer
+
 
 func on_player_finished_dying() -> void:
 	if Global.is_game_over: return
 	Global.game_ready.emit()
+
+
+func on_level_cleared() -> void:
+	switch_to_next_level_timer.start()
+
+
+func on_game_over() -> void:
+	switch_to_next_level_timer.start()
 
 
 func _ready() -> void:
@@ -17,3 +27,9 @@ func _ready() -> void:
 	
 	Global.player_finished_dying.connect(on_player_finished_dying)
 	Global.game_ready.emit()
+	Global.level_cleared.connect(on_level_cleared)
+	Global.game_over.connect(on_game_over)
+
+
+func _on_switch_to_next_level_timer_timeout() -> void:
+	get_tree().change_scene_to_file(next_level_to_load_file_path)
