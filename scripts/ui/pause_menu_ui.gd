d52fedb99065f555f5aed33365995dfc98270bb6
Alix <alix@github.com>
2024-03-22 15:27:20 +0100

    Feat: Add PauseMenuUI
    Many of its components are saved separatly for reusability

---
+++ scripts/ui/pause_menu_ui.gd
@@ -0,0 +1,24 @@
+extends Panel
+class_name PauseMenuUI
+
+
+func _ready() -> void:
+	self.hide()
+
+
+func _unhandled_key_input(_event: InputEvent) -> void:
+	if Global.is_game_over: return
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().get_root().set_input_as_handled()
+		self.set_visible(not self.is_visible())
+	
+		if self.is_visible():
+			get_tree().set_pause(true)
+		else:
+			get_tree().set_pause(false)
+
+
+func _on_resume_button_pressed() -> void:
+	self.set_visible(false)
+	get_tree().set_pause(false)
