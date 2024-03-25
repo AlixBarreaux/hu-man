4373966a9dbd4dcbfcc7b4ed3be388cda4a94fd7
Alix <alix@github.com>
2024-03-25 22:15:17 +0100

    Feat: Add fullscreen on start
    Only if the game is not in debug mode

--- autoloads/scripts/settings.gd
+++ autoloads/scripts/settings.gd
@@ -4,3 +4,5 @@ extends Node
 
 func _ready() -> void:
 	TranslationServer.set_locale(OS.get_locale())
+	if not OS.is_debug_build():
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
